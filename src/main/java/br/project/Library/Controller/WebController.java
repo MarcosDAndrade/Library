5e2d70b99276b2f2427d2f61c1f6644ff4484bb4
Marcos Andrade <Marcosp.360l@gmail.com>
2024-03-12 18:01:00 -0300

    webpage 0.3

--- src/main/java/br/project/Library/Controller/WebController.java
+++ src/main/java/br/project/Library/Controller/WebController.java
@@ -1,2 +1,16 @@
-package br.project.Library.Controller;public class WebController {
+package br.project.Library.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/library")
+public class WebController {
+
+    @GetMapping("/index")
+    public String firstPage(){
+        return "index";
+    }
 }
