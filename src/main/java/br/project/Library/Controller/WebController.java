fc4988df4a85525bf5927fefe363531c852d5c4b
Marcos Andrade <Marcosp.360l@gmail.com>
2024-03-17 22:14:09 -0300

    website 0.5

--- src/main/java/br/project/Library/Controller/WebController.java
+++ src/main/java/br/project/Library/Controller/WebController.java
@@ -2,6 +2,7 @@ package br.project.Library.Controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebController {
 
     @GetMapping("/index")
-    public String firstPage(){
+    public String index(Model model){
+        model.addAttribute("nome", "Marcos");
+
         return "index";
     }
 }
--- src/main/java/br/project/Library/Entities/User.java
+++ src/main/java/br/project/Library/Entities/User.java
@@ -1,2 +1,51 @@
-package br.project.Library.Entities;public class User {
+package br.project.Library.Entities;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String userName;
+
+    private String password;
+
+
+    public User(String userName, Long id, String password) {
+        this.userName = userName;
+        this.id = id;
+        this.password = password;
+    }
+
+    public User(){
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
